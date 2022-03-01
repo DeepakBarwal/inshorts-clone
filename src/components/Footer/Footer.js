8340af7fbb064e285b91a3e123e030510f9ef0c9
DeepakBarwal <deepakbarwal98@gmail.com>
2022-03-01 15:17:53 +0530

    added rel='noreferrer' to all <a>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,6 +9,7 @@ const Footer = () => {
         <a
           href="https://www.linkedin.com/in/deepak-barwal-67283215a/"
           target="_blank"
+          rel="noreferrer"
         >
           Deepak Barwal
         </a>
@@ -17,12 +18,17 @@ const Footer = () => {
       <hr style={{ width: '90%' }} />
 
       <div className="iconContainer">
-        <a href="https://twitter.com/IamBarwal" target="_blank">
+        <a
+          href="https://twitter.com/IamBarwal"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fab fa-twitter-square fa-2x"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/deepak-barwal-67283215a/"
           target="_blank"
+          rel="noreferrer"
         >
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
